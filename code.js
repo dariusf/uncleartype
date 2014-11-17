a7a12515aa323ddd8f76c3cfeda988f3535447ff
Darius Foo <darius.foo.tw@gmail.com>
2014-11-17 12:38:11 +0800

    Initial working prototype!

--- code.js
+++ code.js
@@ -1,7 +1,4 @@
 
-
-// var $ = document.querySelectorAll.bind(document);
-
 var pTag = $("#text");
 
 pTag.html(pTag.html().split(/\s+/g).map(function (t) {
@@ -10,20 +7,14 @@ pTag.html(pTag.html().split(/\s+/g).map(function (t) {
 
 var slice = [].slice;
 
-// console.log(pTag.children());
-
 var nodes = pTag.children().filter(function (element) {
     return element.data !== " ";
 });
-
-// console.log(nodes);
 
 var index = 0;
 
 function highlight(index) {
     nodes.css("color", "#000000");
-    // nodes.forEach(function (node) {
-    // });
     $(nodes[index]).css("color", "#ff0000");
 }
 
@@ -32,24 +23,13 @@ function getText(index) {
 }
 
 highlight(0);
-// console.log(getText(0));
-
-// setTimeout(function () {
-//     highlight(1);
-// }, 1000);
 
 function startsWith(aa, a) {
     return aa.substr(0, a.length) === a;
 }
 
 $('#field').on('input', function (e) {
-    // e.which == charcode of the pressed key, keyCode might be clearer
-    // console.log(e);
-    // console.log($('#field').val());
-
     var current = $('#field').val();
-
-    // console.log(current, getText(index), startsWith(getText(index), current));
 
     // if current is not a prefix of the next word, signal error
     if (!startsWith(getText(index), current)) {
@@ -61,24 +41,25 @@ $('#field').on('input', function (e) {
 
 var SPACE = 32;
 
-$('#field').on('keydown', function (e) {
+$('#field').on('keypress', function (e) {
     var current = $('#field').val().trim();
 
-    if (e.keyCode === SPACE) {
+    if (index === nodes.length-1) {
+        if (current + String.fromCharCode(e.keyCode) === getText(index)) {
+            e.preventDefault();
+            end();
+        }
+    } else if (e.keyCode === SPACE) {
         if (current === getText(index)) {
             ++index;
             e.preventDefault();
             $('#field').val("");
-
-            if (index === nodes.length) {
-                end();
-            }
         }
         highlight(index);
     }
 });
 
 function end() {
-    $('#field').off('input').off('keydown');
+    $('#field').off('input').off('keydown').val("");
     $('#text').html("Done!");
 }
